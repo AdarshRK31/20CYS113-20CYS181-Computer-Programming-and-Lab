8bd3a7f0586c937d67045a4cbb179cd74d40eb25
Adarsh RK <116828908+AdarshRK31@users.noreply.github.com>
2023-06-22 05:50:19 -0400

    Q10_uncorrected

---
+++ complex.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+struct complex{
+	int realpart;
+	int imaginarypart;
+	int value;
+}c,d;
+int main(){
+	int i;
+	printf("enter the value for real part");
+	scanf("%d",[c].realpart);
+	printf("Enter the value for imaginary part");
+	scanf("%d",[d].imaginarypart);
+	printf("Enter the value for 2nd real part");
+	scanf("%d",[c].realpart);
+	printf("Enter the value for 2nd imaginary part");
+	scanf("%d",[d].imaginarypart);
+	value=(realpart) * (i * imaginarypart);
+	return 0;
+}
+
